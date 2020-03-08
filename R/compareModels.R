f284d8ffc5987ca179b0ef0899e6d141b6ab60b3
Gordon McDonald <gordon.mcdonald@sydney.edu.au>
2020-03-08 17:09:24 +1100

    add no test data as an option

--- R/compareModels.R
+++ R/compareModels.R
@@ -10,7 +10,7 @@
 #'               test_data = ames_test_engineered,
 #'               model_order = "RMSE")
 #' @export
-compareModels <- function(model_list, test_data, model_order = "RMSE", title = "Comparing model metrics"){
+compareModels <- function(model_list, test_data = NULL, model_order = "RMSE", title = "Comparing model metrics"){
 
   #Get outcome variable name
   outcome_var <- as.character(
@@ -30,33 +30,38 @@ compareModels <- function(model_list, test_data, model_order = "RMSE", title = "
     dplyr::bind_rows(.id = 'model') %>%
     dplyr::mutate(test_train = "1. train")
 
-  # Get testing data metrics
+  # Get testing data metrics if test data was supplied
+  if (!is.null(test_data)){
 
-  allPreds<-lapply(model_list,predict,test_data) %>%
-    unlist() %>%
-    matrix(.,ncol = length(model_list)) %>%
-    {colnames(.)<-names(model_list);
-    .}
+    allPreds<-lapply(model_list,predict,test_data) %>%
+      unlist() %>%
+      matrix(.,ncol = length(model_list)) %>%
+      {colnames(.)<-names(model_list);
+      .}
 
-  tidy_test_metrics <- apply(allPreds,
-                             2,
-                             caret::postResample,
-                             obs = test_data[[outcome_var]]) %>%
-    dplyr::as_tibble(rownames = "metric") %>%
-    tidyr::pivot_longer(-metric,names_to = c("model")) %>%
-    dplyr::mutate(test_train = "2. test",
-                  Resample = "test")
+    tidy_test_metrics <- apply(allPreds,
+                               2,
+                               caret::postResample,
+                               obs = test_data[[outcome_var]]) %>%
+      dplyr::as_tibble(rownames = "metric") %>%
+      tidyr::pivot_longer(-metric,names_to = c("model")) %>%
+      dplyr::mutate(test_train = "2. test",
+                    Resample = "test")
 
-  # bind metrics together
-  metrics <- rbind(tidy_test_metrics,
-                   tidy_train_metrics)
+    # bind metrics together
+    metrics <- rbind(tidy_test_metrics,
+                     tidy_train_metrics)
+
+  } else {
+    metrics <- tidy_train_metrics
+  }
 
   #summary of metrics for ordering
   sm <- metrics %>%
     dplyr::group_by(model,metric) %>%
-    dplyr::summarise(mean_metric = mean(value)) %>%
+    dplyr::summarise(median_metric = median(value)) %>%
     dplyr::ungroup() %>%
-    dplyr::arrange(metric,mean_metric)
+    dplyr::arrange(metric,median_metric)
 
   model_order <- sm %>%
     dplyr::filter(metric == model_order) %>%
@@ -66,13 +71,13 @@ compareModels <- function(model_list, test_data, model_order = "RMSE", title = "
   p <- metrics %>%
     dplyr::mutate(model = factor(model, levels = model_order, ordered = T)) %>%
     ggplot2::ggplot(aes(y = value,
-               x = model,
-               color = test_train))+
+                        x = model,
+                        color = test_train))+
     ggplot2::geom_boxplot()+
     ggplot2::facet_grid(metric~., scales = "free")+
     ggplot2::theme(axis.text.x = element_text(angle = 90,hjust = 1))+
     ggplot2::labs(x = "Model",
-         y = "Metric",
-         title = title,
-         color = "Train CV / Test")
+                  y = "Metric",
+                  title = title,
+                  color = "Train CV / Test")
 }
