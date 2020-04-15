6fc8689ce55d66992236f6e8ddc6abe7807573fc
Gordon McDonald <gordon.mcdonald@sydney.edu.au>
2020-04-15 19:44:55 +1000

    fixed hard coded variable names to make generic

--- R/eval_classifier.R
+++ R/eval_classifier.R
@@ -2,9 +2,17 @@
 #'
 #' @param trained_model A caret classifier model.
 #' @param test_data The test set of data for evaluation.
+#' @param positive The name of the positive class. Defaults to the first level if positive = NULL.
 #' @return a dataframe is returned and a ggplot is printed as a side effect.
 #' @export
-eval_classifier <- function(trained_model, test_data) {
+eval_classifier <- function(trained_model, test_data, positive = NULL) {
+
+  if (!is.null(positive)){
+    if (!(positive %in% trained_model$levels)){
+      positive <- NULL
+      warning("The specified positive class is not in the outcome variable. Defaulting to the first level in the outcome variable.")
+    }
+  }
 
   outcome_var <- as.character(
     trained_model$terms[[2]]
@@ -16,15 +24,14 @@ eval_classifier <- function(trained_model, test_data) {
   y_pred <- predict(trained_model, test_data, type = "raw")
   y_pred_prob <- predict(trained_model, test_data, type = "prob")
   # spit out the confusion matrix on the test set
-  confm <- caret::confusionMatrix(data = y_pred, y_test, positive="diabetes")
+  confm <- caret::confusionMatrix(data = y_pred, y_test, positive=positive)
   print(confm)
+  # get the positive and negative class names
 
+  pos_name <- confm$positive
+  neg_name <- setdiff(names(y_pred_prob),pos_name)
   # make test predictions data frame
-  tdf <- tibble(y_pred,
-                diabetes = y_pred_prob$diabetes, #B
-                normal = y_pred_prob$normal,
-                y_test = y_test
-  )
+  tdf <- cbind(y_pred, y_pred_prob, y_test)
 
   # Select a parameter setting if random forest
   if (trained_model$method == "rf") {
@@ -35,7 +42,7 @@ eval_classifier <- function(trained_model, test_data) {
   }
 
   # Get the test set AUC:
-  test_auc <- pROC::auc(y_test, y_pred_prob$diabetes)
+  test_auc <- pROC::auc((y_test==pos_name)*1, y_pred_prob[[pos_name]])
 
   metrics <- data.frame(
     accuracy = confm[['overall']]['Accuracy'],
@@ -47,9 +54,13 @@ eval_classifier <- function(trained_model, test_data) {
     mutate(f1 = 2*((precision*recall)/(precision+recall)))
 
   # plot train ROC in red
-  p <- ggplot(
-    selected_pred,
-    aes(m = normal, d = obs, col = "train")
+  selected_pred$D <- 1*(as.character(selected_pred$obs)==neg_name)
+  selected_pred$colour <- "train"
+  test_D <- 1*(as.character(y_test)==neg_name)
+  test_colour <- rep("test",length(test_D))
+
+  p <- ggplot(selected_pred,
+              aes_string(m = neg_name, d = "D", col = "colour")
   ) +
     plotROC::geom_roc(
       hjust = -0.4,
@@ -60,7 +71,7 @@ eval_classifier <- function(trained_model, test_data) {
       hjust = -0.4,
       vjust = 1.5,
       data = tdf,
-      mapping = aes(m = normal, d = y_test, col = "test")
+      mapping = aes_string(m = neg_name, d = "test_D", col = "test_colour")
     ) +
     scale_color_manual(name="Dataset",
                        values=c(train="red", test="blue")) +
@@ -74,4 +85,5 @@ eval_classifier <- function(trained_model, test_data) {
   print(p)
 
   return(invisible(metrics))
+
 }
